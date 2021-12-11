e59f9d6717a5d996058f7ec2c762df9674b50343
suyeon <namtndus@gmail.com>
2021-12-11 23:14:04 +0900

    서버 초기 세팅

---
+++ src/main/java/com/namtndus/Bookmetting/BookmettingApplication.java
@@ -0,0 +1,13 @@
+package com.namtndus.Bookmetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namtndus/Bookmetting/BookmettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namtndus.Bookmetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
